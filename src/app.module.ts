25295a7c02a2c20877dba9f292ed4d0cd5ea084e
GEORGY <goshana87@mail.ru>
2021-07-04 22:27:30 +0300

    Create app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// import { TrackModule } from './track/track.module';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { FileModule } from './file/file.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ServeStaticModule.forRoot({
+            rootPath: path.resolve(__dirname, 'static'),
+          }),
+        // MongooseModule.forRoot(
+        //     'mongodb+srv://admin:1987toyuiui@cluster0.erfjs.mongodb.net/SPLICE?retryWrites=true&w=majority',
+        // ),
+        // TrackModule,
+        // FileModule,
+    ],
 })
 export class AppModule {}

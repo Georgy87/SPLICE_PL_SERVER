6f1ea7fbf417d59d5f8eaecb79c46fe2041902f8
GEORGY <goshana87@mail.ru>
2022-02-11 16:08:58 +0300

    test500

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 const start = async () => {
     try {
-        const PORT = process.env.PORT;
+        const PORT = process.env.PORT || 5000;
         const app = await NestFactory.create(AppModule);
         app.enableCors();
         await app.listen(PORT, () => {
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,36 +3,38 @@ import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { FileInterceptor } from '@nestjs/platform-express';
 
-@Controller('api/users')
+@Controller()
 export class UsersController {
 	constructor(private readonly userService: UsersService) {}
-
-	@UseGuards(JwtAuthGuard)
-	@Put('email')
-	changeEmail(@Req() req: any, @Body('email') email: string) {
-		const userId = req.user.id;
-		return this.userService.changeEmail(userId, email);
+	getHello(): string {
+		return 'Hello World!';
 	}
+	// @UseGuards(JwtAuthGuard)
+	// @Put('email')
+	// changeEmail(@Req() req: any, @Body('email') email: string) {
+	// 	const userId = req.user.id;
+	// 	return this.userService.changeEmail(userId, email);
+	// }
 
-	@UseGuards(JwtAuthGuard)
-	@Put('fullname')
-	changeName(@Req() req: any, @Body('fullname') fullname: string) {
-		const userId = req.user.id;
-		return this.userService.changeName(userId, fullname);
-	}
+	// @UseGuards(JwtAuthGuard)
+	// @Put('fullname')
+	// changeName(@Req() req: any, @Body('fullname') fullname: string) {
+	// 	const userId = req.user.id;
+	// 	return this.userService.changeName(userId, fullname);
+	// }
 
-	@UseGuards(JwtAuthGuard)
-	@Get('liked-samples')
-	getLikedSamples(@Req() req: any) {
-		const userId = req.user.id;
-		return this.userService.getLikedSamples(userId);
-	}
+	// @UseGuards(JwtAuthGuard)
+	// @Get('liked-samples')
+	// getLikedSamples(@Req() req: any) {
+	// 	const userId = req.user.id;
+	// 	return this.userService.getLikedSamples(userId);
+	// }
 
-	@UseGuards(JwtAuthGuard)
-	@Put('avatar')
-	@UseInterceptors(FileInterceptor('file'))
-	createAvatar(@Req() req: any, @UploadedFile() file: Express.Multer.File) {
-		const userId = req.user.id;
-		return this.userService.createAvatar(userId, file);
-	}
+	// @UseGuards(JwtAuthGuard)
+	// @Put('avatar')
+	// @UseInterceptors(FileInterceptor('file'))
+	// createAvatar(@Req() req: any, @UploadedFile() file: Express.Multer.File) {
+	// 	const userId = req.user.id;
+	// 	return this.userService.createAvatar(userId, file);
+	// }
 }

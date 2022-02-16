6bcb9f930b4982c3e617a35640ddfe82c69babf3
GEORGY <goshana87@mail.ru>
2022-02-16 21:24:11 +0300

    Deploy for heroku

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-file
+
 import { Samples } from '../samples/schema/samples.schema';
 import { LoginUserDto } from './dto/login-user.dto';
 import { User } from './schema/user.schema';

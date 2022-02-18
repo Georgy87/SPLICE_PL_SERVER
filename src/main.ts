bc1bf819f0110748f061493dba40a94bdc2f99c9
GEORGY <goshana87@mail.ru>
2022-02-18 22:07:22 +0300

    Deploy to heroku

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const start = async () => {
         const PORT = process.env.PORT || 5000;
         const app = await NestFactory.create(AppModule);
         app.enableCors();
-      
+        //tesdtsdfsdfsdf
         await app.listen(PORT, () => {
             console.log(`server started on PORT ${PORT}`);
         });
